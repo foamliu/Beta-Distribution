019a89d79d49dc83ed64bf5507a84e3854d948c5
unknown <foamliu@yeah.net>
2018-11-15 16:57:21 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Beta-Distribution
+
+![image]()
--- demo.py
+++ demo.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
-a = 1
-b = 10
+a = 3
+b = 7
 x = np.arange(0.01, 1, 0.01)
 y = stats.beta.pdf(x, a, b)
 plt.plot(x, y)
 plt.title('Beta: a=%.1f, b=%.1f' % (a, b))
 plt.xlabel('x')
 plt.ylabel('Probability density')
+plt.savefig('beta-dist.png')
 plt.show()
